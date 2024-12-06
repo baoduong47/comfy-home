4c0df981f9f8118840656b587c56b2a001b7908c
baoduong47 <bduong1497@gmail.com>
2024-12-06 14:53:36 -0500

    Completed sidebar component with responsive design

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,8 @@
-//   ATTENTION!!!!!!!!!!!
-//   I SWITCHED TO PERMANENT DOMAIN
-//   DATA IS THE SAME JUST A DIFFERENT URL,
-//   DOES NOT AFFECT PROJECT FUNCTIONALITY
-
-const allProductsUrl = 'https://www.course-api.com/javascript-store-products';
+const allProductsUrl = "https://www.course-api.com/javascript-store-products";
 // temporary single product
 // 'https://www.course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
 const singleProductUrl =
-  'https://www.course-api.com/javascript-store-single-product';
+  "https://www.course-api.com/javascript-store-single-product";
 
 const getElement = (selection) => {
   const element = document.querySelector(selection);
